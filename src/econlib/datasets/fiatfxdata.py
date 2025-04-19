e15a098b2a022c1388c314a6937090ab707f0ac4
Vivek <vivegau@gmail.com>
2025-04-18 21:59:52 -0400

    updates

--- src/econlib/datasets/fiatfxdata.py
+++ src/econlib/datasets/fiatfxdata.py
@@ -1,4 +1,11 @@
-API_KEY = "17c2b9fd0b3746fbbe13f0b0479a4bbf"
 
-API_EP = f"https://api.exchangeratesapi.io/v1/latest?access_key={API_KEY}"
 
+class EndpointFactory:
+
+    BASE_URL = "https://api.exchangeratesapi.io/v1/"
+    API_KEY = "17c2b9fd0b3746fbbe13f0b0479a4bbf"
+
+    @classmethod
+    def get_historical_rates_endpoint(cls, date, base, quote_symbols):
+        """Get Historical End points"""
+        return f"{cls.BASE_URL}/{date.strftime("%Y-%m-%d")}?access_key={cls.API_KEY}&base=GBP&symbols={quote_symbols.join(",")}"
--- src/econlib/statsutils/primary.py
+++ src/econlib/statsutils/primary.py
@@ -2,11 +2,25 @@ import logging
 import httpx
 from econlib.datasets import fiatfxdata
 import tabulate
+from datetime import datetime
+from dateutil.rrule import rrule, DAILY
+import pandas as pd
 
 log = logging.getLogger(__name__) 
 
-def get_primary_stats(base_ccy, to_ccy):
+def get_primary_stats(start_date, end_date, base_ccy, to_ccy):
     log.info("Running Primary Stats for Fiat %s", to_ccy)
-    data = httpx.get(f"{fiatfxdata.API_KEY}&base={base_ccy}&symbols={to_ccy}").json()
+    dates = rrule(DAILY, dtstart=start_date, until=end_date)
+    historical_data = []
+    for date in dates:
+        response = httpx.get(fiatfxdata.EndpointFactory.get_historical_rates_endpoint(date, base_ccy, [to_ccy]), timeout=None)
+        data = response.json()
+        for ccy, rate in data["rates"]:
+            historical_data.append({"date": date, "ccy": ccy, "rate": rate})
+
+    historical_df = pd.DataFrame(historical_data)
+    import pdb 
+    pdb.set_trace()
+    _ = 2
 
     
