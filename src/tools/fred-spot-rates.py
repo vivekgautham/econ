c80c2cf9e3b5dd229d6414e9cd0c47244049a464
Vivek <vivegau@gmail.com>
2025-04-27 17:23:32 -0400

    updates

--- src/tools/fred-spot-rates.py
+++ src/tools/fred-spot-rates.py
@@ -1,11 +1,12 @@
 #!/usr/local/bin/economics/bin/python
+import datetime
 
 from econlib.datasets.fred.connect import fetch_spot_rates
 
 
 def show_spot_rates():
     """Show Spot Rates"""
-    fetch_spot_rates()
+    fetch_spot_rates(datetime.date(2025, 4, 1), datetime.date(2025, 4, 18))
 
 
 if __name__ == "__main__":
