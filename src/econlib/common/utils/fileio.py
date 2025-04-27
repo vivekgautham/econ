7c8dfa1065f289315a4888de6aa479c7588bdc9a
Vivek <vivegau@gmail.com>
2025-04-27 19:24:53 -0400

    updates

---
+++ src/econlib/common/utils/fileio.py
@@ -0,0 +1,36 @@
+import asyncio
+import os
+import random
+import tempfile
+
+import aiofiles
+
+
+async def async_writer(file_paths, lines):
+    for path in file_paths:
+        async with aiofiles.open(path, mode="w") as f:
+            await f.write("\n".join(lines))
+
+
+def sync_writer(file_paths, lines):
+    for path in file_paths:
+        with open(path, mode="w") as f:
+            f.write("\n".join(lines))
+
+
+def async_temp_dir_writer(num_of_files, num_of_lines):
+    with tempfile.TemporaryDirectory(suffix="_async") as tmp_dir:
+        asyncio.run(
+            async_writer(
+                [os.path.join(tmp_dir, f"file{i}") for i in range(num_of_files)],
+                [random.random() for i in range(num_of_lines)],
+            )
+        )
+
+
+def sync_temp_dir_writer(num_of_files, num_of_lines):
+    with tempfile.TemporaryDirectory(suffix="_sync") as tmp_dir:
+        sync_writer(
+            [os.path.join(tmp_dir, f"file{i}") for i in range(num_of_files)],
+            [random.random() for i in range(num_of_lines)],
+        )
