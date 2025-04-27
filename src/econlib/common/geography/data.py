8c4aab4bec5d029b4ac875cd24cdf3c41a405557
Vivek <vivegau@gmail.com>
2025-04-26 21:24:53 -0400

    updates

--- src/econlib/common/geography/data.py
+++ src/econlib/common/geography/data.py
@@ -7,17 +7,18 @@ GEO_STATIC_DATA_BASE_URL = (
 )
 
 
-@functools.cache
-def get_countries():
-    countries_file = "countries.csv"
-    url = f"{GEO_STATIC_DATA_BASE_URL}{countries_file}"
+def _get_geo_static_data(file_name: str) -> pd.DataFrame:
+
+    url = f"{GEO_STATIC_DATA_BASE_URL}{file_name}"
     df = pd.read_csv(url)
     return df
+
+
+@functools.cache
+def get_countries():
+    return _get_geo_static_data("countries.csv")
 
 
 @functools.cache
 def get_regions():
-    countries_file = "countries.csv"
-    url = f"{GEO_STATIC_DATA_BASE_URL}{countries_file}"
-    df = pd.read_csv(url)
-    return df
+    return _get_geo_static_data("regions.csv")
