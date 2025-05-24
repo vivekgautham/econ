fb63ea02da0e95ecd1726b2f588c9ae72ab87349
Vivek <vivegau@gmail.com>
2025-05-23 22:18:39 -0400

    updates

--- src/tools/fred-spot-rates.py
+++ src/tools/fred-spot-rates.py
@@ -1,13 +1,21 @@
 #!/usr/local/bin/economics/bin/python
 import datetime
 
+from rich.prompt import Prompt
+
 from econlib.datasets.fred.connect import fetch_spot_rates
 
 
-def show_spot_rates():
+def show_spot_rates(start_date: datetime.date, end_date: datetime.date):
     """Show Spot Rates"""
     fetch_spot_rates(datetime.date(2025, 4, 13), datetime.date(2025, 4, 18))
 
 
 if __name__ == "__main__":
-    show_spot_rates()
+    start_date_response = Prompt.ask("Enter Start Date (YYYYMMDD): ")
+    end_date_response = Prompt.ask("Enter End Date (YYYYMMDD): ")
+    print(start_date_response, end_date_response)
+    show_spot_rates(
+        datetime.datetime.strptime(start_date_response.strip(), "%Y%m%d").date(),
+        datetime.datetime.strptime(start_date_response.strip(), "%Y%m%d").date(),
+    )
