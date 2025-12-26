15609de70b3232e2c40012c0e86ebaeb1b76610d
Vivek <vivegau@gmail.com>
2025-12-25 20:43:52 -0500

    updates

--- src/tools/world-view.py
+++ src/tools/world-view.py
@@ -1,46 +1,71 @@
 #!/usr/local/bin/economics/bin/python
+"""World view.
+
+A command-line tool to view information about continents, countries, and regions.
+"""
 import argparse
 
 from econlib.common.geography.models import World
 
 
-def region_view(country, args):
-    if args.rgc:
-        region = country.get_regions_from_code(args.rgc)
-        region.short_summary()
+def view(entity, args):
+    """View an entity.
+
+    Args:
+        entity: The entity to view.
+        args: The command-line arguments.
+    """
+    if args.continent:
+        continent = entity.get_continent_from_code(args.continent)
+        if args.country:
+            country = continent.get_country_from_code(args.country)
+            if args.region:
+                region = country.get_regions_from_code(args.region)
+                if args.verbose:
+                    region.summary()
+                else:
+                    region.short_summary()
+            elif args.verbose:
+                country.summary()
+            else:
+                country.short_summary()
+        elif args.verbose:
+            continent.summary()
+        else:
+            continent.short_summary()
+    elif args.verbose:
+        entity.summary()
     else:
-        country.short_summary()
+        entity.short_summary()
 
 
-def country_view(continent, args):
-    if args.ctc:
-        country = continent.get_country_from_code(args.ctc)
-        region_view(country, args)
-    else:
-        continent.short_summary()
+def main():
+    """Run the world view script."""
+    parser = argparse.ArgumentParser(
+        description="A command-line tool to view information about continents, countries, and regions."
+    )
 
-
-def continent_view(world, args):
-    if args.coc:
-        continent = world.get_continent_from_code(args.coc)
-        country_view(continent, args)
-    else:
-        world.short_summary()
-
-
-def world_view(args):
-    """Run World View per arguments"""
-    world = World()
-    continent_view(world, args)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="World view")
-
-    parser.add_argument("-coc", "-continentcode", help="continent code", required=False)
-    parser.add_argument("-ctc", "-countrycode", help="country code", required=False)
-    parser.add_argument("-rgc", "-regioncode", help="region code", required=False)
+    parser.add_argument(
+        "--continent", help="The code of the continent to view.", required=False
+    )
+    parser.add_argument(
+        "--country", help="The code of the country to view.", required=False
+    )
+    parser.add_argument(
+        "--region", help="The code of the region to view.", required=False
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="View the full details of an entity.",
+        action="store_true",
+    )
 
     args = parser.parse_args()
 
-    world_view(args)
+    world = World()
+    view(world, args)
+
+
+if __name__ == "__main__":
+    main()
