d2891221aa38a1e8ef15d624cbe9418e4fd90ade
Vivek <vivegau@gmail.com>
2025-12-26 20:13:44 -0500

    updates

--- src/econlib/datasets/fx/primary.py
+++ src/econlib/datasets/fx/primary.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 import pandas as pd
 import tabulate
@@ -11,10 +12,12 @@ log = logging.getLogger(__name__)
 def get_primary_fiat_fx_stats(from_ccy: str, to_ccy: str) -> pd.DataFrame:
     log.info("Running Primary Stats for Fiat FXs %s", to_ccy)
     historical_df_to_ccy = fiatfxdata.get_historical_fiatfx_data(to_ccy)
+    log.info("Sleeping for 10 secs")
+    time.sleep(10)
     historical_df_base_ccy = fiatfxdata.get_historical_fiatfx_data(from_ccy)
     final_df = historical_df_base_ccy.merge(historical_df_to_ccy, on=["date"])
     final_df["quote_ccy"] = to_ccy
-    final_df["rate"] = final_df["rate_x"] / final_df["rate_y"]
+    final_df["rate"] = final_df["rate_y"] / final_df["rate_x"]
     pivot_view = pd.DataFrame(
         final_df.pivot(index="quote_ccy", columns="date", values="rate")
         .reset_index()
